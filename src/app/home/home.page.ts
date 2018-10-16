20b41070dbc4fcc1f0ddcdd272d9c3dd8bb9fb42
Jhonatan Morais <jhonatanvinicius@gmail.com>
2018-10-15 23:57:02 -0300

    dia 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Jogo } from '../models/Jogo';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  private listaDeJogos:Jogo[] = new Array();
+
+
+  constructor(){
+    this.listaDeJogos.push(
+      {
+        concurso:1234,
+        dataSorteio:'25/08/1986',
+        nome: 'Lotomania'
+      },{
+        concurso:4321,
+        dataSorteio:'27/08/1986',
+        nome: 'Mega Sena'
+      }
+    )
+
+
+
+
+  }
+
 
 }
---
+++ src/app/models/Jogo.ts
@@ -0,0 +1,10 @@
+export interface Jogo{
+    concurso:number;
+    nome:string;
+    dataSorteio:string;
+    //dezenas:number[];
+    //quantidadeAcertos:number;
+    //ganhador:boolean;
+    //arquivado:boolean;
+    
+}
