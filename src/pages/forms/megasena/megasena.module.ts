17f4dd5bfa3db2efd4a542aa4ef627b4e7e4d1ee
Jhonatan Morais <jhonatanvinicius@gmail.com>
2018-10-16 23:09:32 -0300

    formulario em construção

---
+++ src/pages/forms/megasena/megasena.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MegasenaPage } from './megasena';
+
+@NgModule({
+  declarations: [
+    MegasenaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MegasenaPage),
+  ],
+})
+export class MegasenaPageModule {}
