4db2b8ca0775760b4702d4410a8c9a207e089a6d
Jhonatan Morais <jhonatanvinicius@gmail.com>
2018-10-19 08:55:46 -0300

    parei iterando a grid do cdastroacadastro

--- src/pages/forms/megasena/megasena.ts
+++ src/pages/forms/megasena/megasena.ts
@@ -8,11 +8,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MegasenaPage {
 
+  dezenasSelecionadas:boolean[] = new Array();
+  numberLimit:number = 60;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MegasenaPage');
+    for(let i = 1; i < this.numberLimit; i++  ){
+        this.dezenasSelecionadas[i] = false;
+    }  
+  }
+
+  toggleSelect(dezena:number){
+       
+      if(this.dezenasSelecionadas[dezena]){
+        this.dezenasSelecionadas[dezena] = false;
+        
+        console.log('removido')
+        console.log(dezena)
+        console.log(this.dezenasSelecionadas)
+      }else{
+
+        this.dezenasSelecionadas[dezena] = true;
+        console.log('add')
+        console.log(dezena)
+        console.log(this.dezenasSelecionadas)
+
+      }
+
+     /* <!-- </ion-col> --> */
+    
+  }
+
+  isSelected(dezena:number){
+    return this.dezenasSelecionadas[dezena]
   }
 
 }
