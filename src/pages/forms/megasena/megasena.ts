04b0079b10ab3f5e23dec1687bca3947a51a5dc2
Jhonatan Morais <jhonatanvinicius@gmail.com>
2018-10-29 22:47:27 -0300

    Form e validação ok. falta o service

--- src/pages/forms/megasena/megasena.ts
+++ src/pages/forms/megasena/megasena.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,14 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MegasenaPage {
 
   dezenasSelecionadas:boolean[] = new Array();
-  valores:number[] = new Array();
-  numberLimit:number = 60;
+  concurso:number;
+  qntConcursos:number;
   teimosinha:boolean = false;
+  qntTeimosinha:number
   recorrente:boolean = false;
+
+  numberLimit:number = 60;
   minimoDezenas:number = 6;
   maximoDezenas:number = 15;
+  valores:number[] = new Array();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      public alertCtrl:AlertController) {
   }
 
   ionViewDidLoad() {
@@ -59,7 +66,7 @@ export class MegasenaPage {
     return this.recorrente;
   }
 
-  get quantidadeMarcado(){
+  get quantidadeMarcado():number{
 
     return this.dezenasSelecionadas.filter(tt => tt).length; 
   }
@@ -81,5 +88,42 @@ export class MegasenaPage {
     return new Array(i);
   }
 
-  
+  cadastrar(){
+
+    try { // respeite a ordem dos IFs ele atende a validação sequêncial
+
+      if (this.quantidadeMarcado < 6 )
+        throw new Error('O jogo mínimo tem 6 dezenas.')
+      
+      if (this.concurso < 1000 || !this.concurso )
+        throw new Error('O número do concurso inválido.')
+      
+      if( this.recorrente )
+        return;
+      
+      if(   !this.recorrente 
+            && !this.teimosinha
+            && !this.qntConcursos 
+        ) throw new Error('Informe a quantidade de concursos.')
+
+      if(  this.teimosinha
+        && !this.qntTeimosinha 
+      ) throw new Error('Informe a quantidade de teimosinhas.')
+
+      // Programar o envio para um ServiceUIFrameContext.getCachedFrameMessage.length./
+
+    } catch (error) {
+        
+      /*this.vibration.vibrate(500);*/
+      this.alertCtrl.create({
+        title:'Atenção',
+        subTitle:  error.message,
+        buttons:[
+          {text: 'ok'}
+        ]
+      }).present();
+      
+    }
+    return;
+  }
 }
